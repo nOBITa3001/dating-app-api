acb3b178822285d672ca6a38a006f01126bfb27d
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2018-02-02 00:08:52 +0100

    + add global exception handler

--- src/DatingApp.API/Startup.cs
+++ src/DatingApp.API/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -59,14 +63,28 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            app.UseCors
-            (
-                a => a.AllowAnyHeader()
-                      .AllowAnyMethod()
-                      .AllowAnyOrigin()
-                      .AllowCredentials()
-            );
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
+            }
+
+            app.UseCors(a => a.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin()
+                              .AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
         }
